ae94ce01f3d63ea492400835f1905aaa99be5e52
Mohammed Dinnunhan (1000033946) <DINNM1@student.op.ac.nz>
2018-06-08 11:24:21 +1200

    Pushing the up to date html and js files, to avoid the conflicts.

---
+++ Firestore/add_user.js
@@ -0,0 +1,87 @@
+function addUser()
+{
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+
+    console.log(email);
+    console.log(password);
+
+    firebase.auth().createUserWithEmailAndPassword(email.value, password.value).then(function(user)
+	{
+		addUserPermissions(user.uid);
+		alert("New user added");
+		sendPasswordReset();	
+		
+		
+	}).catch(function(error)
+	{
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+		console.log(error.message);
+		
+		if (errorCode == 'auth/weak-password') {
+          alert('The password is too weak.');
+        } else {
+          alert(errorMessage);
+        }
+        console.log(error);
+		
+		
+        // ...
+    });
+}
+
+function addUserPermissions(userID)
+{
+	var read = document.getElementById("read").checked;
+	var write = document.getElementById("write").checked;
+	var update = document.getElementById("update").checked;
+	var del = document.getElementById("delete").checked;	
+	
+	db.collection("Users").doc(userID).set({
+    read: read,
+    write: write,
+    update: update,
+	delete: del
+})
+.then(function() {
+    console.log("Document successfully written!");
+})
+.catch(function(error) {
+    console.error("Error writing document: ", error);
+});  
+	
+	
+}
+
+
+function sendPasswordReset() 
+{
+      var email = document.getElementById('email').value;
+      // [START sendpasswordemail]
+      firebase.auth().sendPasswordResetEmail(email).then(function() {
+        // Password Reset Email Sent!
+        // [START_EXCLUDE]
+        alert('Password Reset Email Sent!');
+		document.getElementById("admin_form").reset();
+        // [END_EXCLUDE]
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        if (errorCode == 'auth/invalid-email') {
+          alert(errorMessage);
+        } else if (errorCode == 'auth/user-not-found') {
+          alert(errorMessage);
+        }
+		
+        console.log(error);
+        // [END_EXCLUDE]
+      });
+      // [END sendpasswordemail];
+}
+
+
+
--- Firestore/display_sealion.js
+++ Firestore/display_sealion.js
@@ -2,25 +2,30 @@ var featureMax = 0;
 var imageSrc = "";
 var rowLength;
 
+var sealions = new Map;
 
 //Gets data from firestore and calls create card on each sea lion
+function displaySeaLionTbl()
+{   
+    db.collection("Sea Lions").get().then(function(document) 
+    {        
+        // Store each sea lion in a map to refer to later
+        document.forEach(function(data)
+        {
+            sealions.set(data.id, data.data());
+        });
+
+        document.forEach(createTable);        
+    }); 
+}
 function displaySeaLion()
-{    
+{   
     db.collection("Sea Lions").get().then(function(document) 
     {        
         document.forEach(createCard);
-    });  
-}  
-
-// Gets sealion data matching id from firestore and calls create card
-function displaySeaLionById(id)
-{    
-    //ERROR CHECKING FOR NON EXISTENT ID
-    db.collection("Sea Lions").get().where('id', '==', id).then(function(document) 
-    {
-        document.forEach(createCard);
-    });  
-} 
+        console.log(document);
+    }); 
+}
 
 // Takes a string and makes first letter upper case and replaces _ with a space
 function cap(string) 
@@ -28,38 +33,162 @@ function cap(string)
     return string.charAt(0).toUpperCase() + string.slice(1).replace("_", " ");    
 }
 
-// Take passed in sea lion object and iterates through creating html elements and diplays to screen.
-function createCard(sealionDocument)
-{ 
-    var sealion = sealionDocument.data();
+function createTable(sealion)
+{
+    var sl = sealion.data();
+    
+    var tbl_body = document.getElementById("tbl_body");
 
+    var tbl_row = document.createElement("tr");    
+    tbl_body.appendChild(tbl_row);
+
+    tbl_row.id = sealion.id;
+    tbl_row.onclick = openModal;
+    
+    var name = document.createElement("td");
+    name.innerHTML = sl.name;    
+    var dob = document.createElement("td");
+    dob.innerHTML = sl.dob;
+    var trans = document.createElement("td");
+    trans.innerHTML = sl.transponder;
+    var status = document.createElement("td");
+    status.innerHTML = sl.living_status;
+    var colour = document.createElement("td");
+    colour.innerHTML = sl.colour;
+    var type = document.createElement("td");
+    type.innerHTML = sl.type;
+    var tag_num = document.createElement("td");
+    tag_num.innerHTML = sl.tag_number;
+    var rf_num = document.createElement("td");
+    rf_num.innerHTML = sl.rf_number;
+    
+    tbl_row.appendChild(name);
+    tbl_row.appendChild(dob);
+    tbl_row.appendChild(trans);
+    tbl_row.appendChild(status);
+    tbl_row.appendChild(colour);
+    tbl_row.appendChild(type);
+    tbl_row.appendChild(tag_num);
+    tbl_row.appendChild(rf_num);
+}
+
+// Take passed in sea lion object and iterates through creating html elements and diplays to screen.
+function createCard(key)
+{ 
+    // use sea lion id to get sea lion from map
+    var sealion = sealions.get(key);
+    
+
+    var containerDiv = document.createElement
     //Get ref to display div and create elements
     var displayDiv = document.getElementById("displaySealions");
-    var sealionDiv = document.createElement("div");        
-    
-    sealionDiv.className = "sealionDiv"; 
-    displayDiv.appendChild(sealionDiv);
 
-    //Loop through each sealion and display data
+    var grid = document.createElement("div");
+    var card_container = document.createElement("div");
+    grid.className = "grid-x grid-padding-x";
+    card_container.className = "card";
+    displayDiv.appendChild(card_container);
+    card_container.appendChild(grid);
+
+    var col1cell = document.createElement("div");
+    var col2cell = document.createElement("div");
+    var col3cell = document.createElement("div");
+    var col4cell = document.createElement("div");
+    col1cell.className = "large-3 medium-3 cell";
+    col2cell.className = "large-3 medium-3 cell";
+    col3cell.className = "large-3 medium-3 cell"; 
+    col4cell.className = "large-3 medium-3 cell";   
+
+    grid.appendChild(col1cell);
+    grid.appendChild(col2cell);
+    grid.appendChild(col3cell);
+    grid.appendChild(col4cell);
+
+    //Creating elements for each variable for the sea lion
+
+    //Details
+    var name = document.createElement("p");
+    var gender = document.createElement("p");
+    var transponder = document.createElement("p");
+    var dob = document.createElement("p");
+    var mother = document.createElement("p");
+    var pob = document.createElement("p");
+    //Fill with field data
+    name.innerHTML = cap("Name") + ": " + sealion["name"];
+    gender.innerHTML = cap("Gender") + ": " + sealion["gender"];  
+    transponder.innerHTML = cap("Transponder #") + sealion["transponder"];  
+    dob.innerHTML = cap("DOB") + ": " + sealion["dob"];  
+    mother.innerHTML = cap("Mother") + ": " + sealion["mother"];
+    pob.innerHTML = cap("POB") + ": " + sealion["pob"];
+
+    //Tag details
+    var date_in = document.createElement("p");
+    var type = document.createElement("p");
+    //var desc = document.createElement("p");
+    var tag_number = document.createElement("p");
+    var rf_number = document.createElement("p");
+    var left_out = document.createElement("p");
+    var right_out = document.createElement("p");
+    //Fill with field data
+    date_in.innerHTML = cap("Tag Date In") + ": " + sealion["tag_date_in"];
+    type.innerHTML = cap("Type") + ": " + sealion["type"];  
+    //desc.innerHTML = cap("Transponder #") + sealion.data()["desc"];  
+    tag_number.innerHTML = cap("Tag Number") + ": " + sealion["tag_number"];  
+    rf_number.innerHTML = cap("RFID Number") + ": " + sealion["rf_number"];
+    left_out.innerHTML = cap("Left Tag Out") + ": " + sealion["left_tag_date_out"];  
+    right_out.innerHTML = cap("Right Tag Out") + ": " + sealion["right_tag_date_out"];
+    
+    //Append to appropriate column div
+    //Column 1
+    col1cell.appendChild(name);
+    col1cell.appendChild(gender);
+    col1cell.appendChild(transponder);
+    col1cell.appendChild(dob);
+    col1cell.appendChild(mother);
+    col1cell.appendChild(pob);
+    //Column 2
+    col2cell.appendChild(date_in);
+    col2cell.appendChild(type);
+    col2cell.appendChild(tag_number);
+    col2cell.appendChild(rf_number);
+    col2cell.appendChild(left_out);
+    col2cell.appendChild(right_out);
+
+    //Loop through each key and if it equals true, print out the variable.
+    //This way only clipped toes are shown.
+    var clipped_title = document.createElement("p");
+    clipped_title.innerHTML = "Clipped toes:";
+    col3cell.appendChild(clipped_title);
     for (var key in sealion) 
-    {        
-        //Create element
-        var fieldElement = document.createElement("p");
-        //Fill with field data 
-        fieldElement.innerHTML = cap(key) + ": " + sealion[key];       
-        //Append to sealion div
-        sealionDiv.appendChild(fieldElement);
-    }   
+    {
+        if(sealion[key] == true)
+        {
+            var fieldElement = document.createElement("p");
+            //Fill with field data 
+            fieldElement.innerHTML = cap(key) + ": Clipped";       
+            //Append to appropriate column div
+            //Column 3
+            col3cell.appendChild(fieldElement);
+        }
+    }  
 
     //Gets all the features associated with sea lion. Updates inner html of element
-    getFeatures(sealionDocument.id, sealionDiv);  
-    // Add element to sea lion div     
+    getFeatures(sealion.id, col4cell);
+    
+    var button = document.createElement('button');
+    button.className = "button update";
+    button.innerHTML = 'Update';
+    button.onclick = function()
+    {
+        window.location='update.html';
+    };
+    col2cell.appendChild(button);    
 }
 
 // Takes in sea lion id, searches features with that id. Fills passed in element with features string
 function getFeatures(id, div)
 { 
-    db.collection("Feature").where("id", "==", id).get().then(function(features) 
+    db.collection("Feature").where("id", "==", id).get().then(function(querySnapshot) 
     {   
         //Create features title 
         var title = document.createElement("p");
@@ -67,18 +196,18 @@ function getFeatures(id, div)
         div.appendChild(title);    
 
         // Go through each feature and create desciption element
-        features.forEach(function(feature) 
+        querySnapshot.forEach(function(doc) 
         {            
             var featureElement = document.createElement("p");
-            featureElement.innerHTML = feature.data().description;
+            featureElement.innerHTML = doc.data().description;
             div.appendChild(featureElement);
 
             // For each image the feature has, create element and fill it with image
-            for (var i = 0; i < feature.data().images; i++)
+            for (var i = 0; i < doc.data().images; i++)
             {
                 var img = document.createElement("img");
                 img.className = "smallImage";
-                getImage(feature.id, i, img);
+                getImage(doc.id, i, img);
                 div.appendChild(img);                
             } 
         }); 
@@ -98,15 +227,32 @@ function getImage(id, index, element)
         element.src = url;        
     });
 }
+
+function openModal(id)
+{
+    //Get sea lion id by getting id of parent of click - same as sealion
+    sealionID = id.target.parentElement.id;
+	// Get the modal
+    var modal = document.getElementById('myModal');  
+    modal.style.display = "block";
+    createCard(sealionID);
+
+    var span = document.getElementsByClassName("close")[0]; 
     
-    
 
-    
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
 
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 
-
-
-
+}
 
 
 
