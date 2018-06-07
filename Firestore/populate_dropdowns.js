befe6d7ec41c4c571503bc43062c93b136b04568
Mohammed Dinnunhan (1000033946) <DINNM1@student.op.ac.nz>
2018-06-08 10:52:19 +1200

    Added dropdown js file.

---
+++ Firestore/populate_dropdowns.js
@@ -0,0 +1,64 @@
+function fillDropdowns()
+{
+	getList("pob");
+	getList("tagcolour");
+	getList("tagtype");
+	getMothers();
+}
+
+function getMothers()
+{
+	db.collection("Sea Lions").get().then(function(document) 
+    {  
+		document.forEach(function(values)
+		{			
+			console.log(values.data());
+			addOptionToDropDown(values.data().name, "mother");					
+		});
+	});
+}
+
+function getList(dropdownName)
+{
+	db.collection(dropdownName).get().then(function(document) 
+    {        
+        document.forEach(function(values)
+		{
+			start(values.data(), dropdownName);					
+		});
+    }); 
+}
+
+function start(data, dropdownName)
+{	
+	for (var key in data)
+	{		
+		addOptionToDropDown(data[key], dropdownName);
+	}
+}
+
+function addOptionToDropDown(value, dropdownName)
+{	
+	var dropDown = document.getElementById(dropdownName);
+	var newOption = document.createElement("option");	
+	console.log(value);
+	newOption.value = value;
+	newOption.text = value;
+	dropDown.appendChild(newOption);
+}
+
+function addToList(button)
+{ 	
+	var buttonName = button.name;
+
+	var newValue = prompt("Enter new "+ buttonName +":");
+	if (newValue == null || newValue == "") {
+        txt = "User cancelled the prompt.";
+    } 
+	else 
+	{
+		var cityRef = db.collection(buttonName).add({buttonName: newValue});		   
+    }
+	document.getElementById(buttonName).innerText = null;
+	getList(buttonName);
+}
