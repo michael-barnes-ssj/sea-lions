a5697051ac96738f531341828fb50604a61cd5a0
Mohammed Dinnunhan (1000033946) <DINNM1@student.op.ac.nz>
2018-06-11 18:41:36 +1200

    Removed quick search bar. Tweaked padding for search page. Removed duplicate add sealion buttons. Added code to allow modal to be closable. Need to fix duplication of modals when clicking out of one and into another.

--- Firestore/display_sealion.js
+++ Firestore/display_sealion.js
@@ -241,23 +241,18 @@ function openModal(id)
     sealionID = id.target.parentElement.id;
 	// Get the modal
     var modal = document.getElementById('myModal');  
+    
     modal.style.display = "block";
     createCard(sealionID);
 
     var span = document.getElementsByClassName("close")[0]; 
-    
 
-    /*// When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
+    //When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
         }
-    }*/
+    }
 
 }
 
